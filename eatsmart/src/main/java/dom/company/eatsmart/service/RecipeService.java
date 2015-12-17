8656e71d2a118161da65d90ef88046786b04078a
Stefan Specht <stefan_specht@gmx.net>
2015-12-17 20:05:39 +0100

    Extended RecipeServer class
    Switched getRecipes to Criteria based query
    Added getRecipe, addRecipe, updateRecipe and removeRecipe methods

--- eatsmart/src/main/java/dom/company/eatsmart/service/RecipeService.java
+++ eatsmart/src/main/java/dom/company/eatsmart/service/RecipeService.java
@@ -3,23 +3,28 @@ package dom.company.eatsmart.service;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import dom.company.eatsmart.model.Recipe;
 
 public class RecipeService {
 	
 	public List<Recipe> getRecipes() {
-		
 		EntityManager entityManager = JpaUtil.getEntityManager();
-		Query query = entityManager.createQuery("Select r from Recipe r");
-		@SuppressWarnings("unchecked")
-		List<Recipe> recipes = query.getResultList();
-		return recipes;
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Recipe> criteriaQuery = criteriaBuilder.createQuery(Recipe.class);
+		TypedQuery<Recipe> typedQuery = entityManager.createQuery(criteriaQuery);
+		
+		return typedQuery.getResultList();
 	}
 	
-	public Recipe addRecipe(Recipe recipe) {
-		
+	public Recipe getRecipe(long id) {
+		return JpaUtil.getEntityManager().find(Recipe.class, id);
+	}
+	
+	public Recipe addRecipe(Recipe recipe) {	
 		EntityManager entityManager = JpaUtil.getEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.persist(recipe);
@@ -27,6 +32,25 @@ public class RecipeService {
 		entityManager.close();
 				
 		return recipe;
+	}
+	
+	public Recipe updateRecipe(Recipe recipe) {
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		if (recipe.getId() <= 0) {
+			return null;
+		}
+		else {
+			return entityManager.merge(recipe);
+		}
+	}
+	
+	public void removeRecipe(long id) {
+		EntityManager entityManager = JpaUtil.getEntityManager();
+		Recipe recipe = entityManager.find(Recipe.class, id);
+		if (recipe != null) {
+			entityManager.remove(recipe);
+		}
 	}
 	
 }
