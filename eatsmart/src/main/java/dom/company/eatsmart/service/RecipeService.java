ee95cc157012bedbfb8578448e974a1da671fa68
Stefan Specht <stefan_specht@gmx.net>
2015-12-17 18:02:47 +0100

    Modified RecipeService getAllRecipes method to read from database

--- eatsmart/src/main/java/dom/company/eatsmart/service/RecipeService.java
+++ eatsmart/src/main/java/dom/company/eatsmart/service/RecipeService.java
@@ -1,36 +1,31 @@
 package dom.company.eatsmart.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import dom.company.eatsmart.model.Recipe;
 
 public class RecipeService {
 	
 	public List<Recipe> getAllRecipes() {
-		Recipe r1 = new Recipe("Spaghetti Carbonara","Nudel kochen, maggi fix dazu, fertig!");
-		Recipe r2 = new Recipe("Spaghetti Gamba","Nudel kochen, gamba rein, maggi fix dazu, fertig!");
 		
-		List<Recipe> recipeList= new ArrayList<>();
-		recipeList.add(r1);
-		recipeList.add(r2);
-		
-		return recipeList;
+		EntityManager entityManager = JpaUtil.getEntityManager();
+		Query query = entityManager.createQuery("Select r from Recipe r");
+		@SuppressWarnings("unchecked")
+		List<Recipe> recipes = query.getResultList();
+		return recipes;
 	}
 	
 	public Recipe addRecipe(Recipe recipe) {
-		
-		//Recipe testRecipe = new Recipe("Spaghetti mit Speck","Speck und Spaghetti in den Topf und warten");
 		
 		EntityManager entityManager = JpaUtil.getEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.persist(recipe);
 		entityManager.getTransaction().commit();
 		entityManager.close();
-		
-		
+				
 		return recipe;
 	}
 	
