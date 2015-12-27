29a6f3392626573b9e40dc1e6f2b35ab6834e533
Stefan Specht <stefan_specht@gmx.net>
2015-12-27 09:32:38 +0100

    Removed un-needed imports from ingredient classes

--- eatsmart/src/main/java/dom/company/eatsmart/resources/IngredientResource.java
+++ eatsmart/src/main/java/dom/company/eatsmart/resources/IngredientResource.java
@@ -1,18 +1,9 @@
 package dom.company.eatsmart.resources;
 
-import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import dom.company.eatsmart.model.Ingredient;
 import dom.company.eatsmart.service.IngredientService;
 
 @Path("/ingredients")
--- eatsmart/src/main/java/dom/company/eatsmart/service/IngredientService.java
+++ eatsmart/src/main/java/dom/company/eatsmart/service/IngredientService.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 
 import dom.company.eatsmart.model.Ingredient;
 
