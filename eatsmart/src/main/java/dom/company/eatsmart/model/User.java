7f41f0f5670133716e79c0169c8c44f7d33001fb
Stefan Specht <stefan_specht@gmx.net>
2016-01-17 14:04:48 +0100

    Annotated email as unique field in user model

--- eatsmart/src/main/java/dom/company/eatsmart/model/User.java
+++ eatsmart/src/main/java/dom/company/eatsmart/model/User.java
@@ -18,10 +18,12 @@ public class User {
 	@Id @GeneratedValue
 	@Column(name="USER_ID")
 	private long id;
+	@Column(unique=true)
+	private String username;
 	private String firstName;
 	private String lastName;
-	private String userName;
 	private String password;
+	@Column(unique=true)
 	private String email;
 	
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "owner")
@@ -45,11 +47,11 @@ public class User {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String userName) {
+		this.username = userName;
 	}
 	public String getPassword() {
 		return password;
