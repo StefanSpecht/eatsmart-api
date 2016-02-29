1fa733054a517a2a626cf8394b27a428fcb26b08
Stefan Specht <stefan_specht@gmx.net>
2016-02-29 02:34:17 +0100

    Finished Product + Prod.Catalogue model incl relations

---
+++ eatsmart/src/main/java/dom/company/eatsmart/model/Product.java
@@ -0,0 +1,85 @@
+package dom.company.eatsmart.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Product {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="PRODUCT_ID")
+	private long id;
+	
+	@NotBlank(message="EAN must not be blank or null")
+	private String ean;
+	
+	@Embedded
+	@NotNull(message="ingredient must not be null")
+	private Ingredient ingredient;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "CATALOGUE_ID")
+	private ProductCatalogue productCatalogue;
+
+	public Product() {
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEan() {
+		return ean;
+	}
+
+	public void setEan(String ean) {
+		this.ean = ean;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public ProductCatalogue getProductCatalogue() {
+		return productCatalogue;
+	}
+
+	public void setProductCatalogue(ProductCatalogue productCatalogue) {
+		this.productCatalogue = productCatalogue;
+		if (!productCatalogue.getProducts().contains(this)) {
+			productCatalogue.getProducts().add(this);
+		}
+	}
+	
+public void removeProductCatalogue() {
+		
+		ProductCatalogue currentProductCatalogue = this.getProductCatalogue();
+		if (currentProductCatalogue != null){
+			this.productCatalogue = null;
+		}
+		if (currentProductCatalogue.getProducts().contains(this)) {
+			currentProductCatalogue.removeProduct(this);
+		}
+	}
+	
+	
+	
+}
--- eatsmart/src/main/java/dom/company/eatsmart/model/ProductCatalogue.java
+++ eatsmart/src/main/java/dom/company/eatsmart/model/ProductCatalogue.java
@@ -1,10 +1,16 @@
 package dom.company.eatsmart.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ProductCatalogue {
@@ -13,4 +19,45 @@ public class ProductCatalogue {
 	@Column(name="PRODUCTCATALOGUE_ID")
 	private long id;
 
+	@OneToMany(targetEntity = Product.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy="productCatalogue")
+	private List<Product> products = new ArrayList<Product>();
+
+	public ProductCatalogue() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+	public void addProduct(Product product) {
+		this.products.add(product);
+		
+		if (product.getProductCatalogue() != this) {
+			product.setProductCatalogue(this);
+		}
+	}
+	
+	public void removeProduct(Product product) {
+		
+		if (this.products.contains(this)) {
+			this.products.remove(product);
+			
+			if (product.getProductCatalogue().equals(this)) {
+				product.removeProductCatalogue();
+			}
+		}
+    }
+		
 }
