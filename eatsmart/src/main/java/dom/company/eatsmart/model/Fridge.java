498bd681ce005f0b57e7c8248216919954654eef
Stefan Specht <stefan_specht@gmx.net>
2016-02-29 02:10:12 +0100

    Added fridge and Stock models + relations

--- eatsmart/src/main/java/dom/company/eatsmart/model/Fridge.java
+++ eatsmart/src/main/java/dom/company/eatsmart/model/Fridge.java
@@ -1,10 +1,16 @@
 package dom.company.eatsmart.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Fridge {
@@ -12,5 +18,48 @@ public class Fridge {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="FRIDGE_ID")
 	private long id;
+	
+	@OneToMany(targetEntity = Stock.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy="fridge")
+	private List<Stock> stocks = new ArrayList<Stock>();
+
+	public Fridge() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<Stock> getStocks() {
+		return stocks;
+	}
+
+	public void setStocks(List<Stock> stocks) {
+		this.stocks = stocks;
+	}
+	
+	public void addStock(Stock stock) {
+		this.stocks.add(stock);
+		
+		if (stock.getFridge() != this) {
+			stock.setFridge(this);
+		}
+	}
+	
+	public void removeStock(Stock stock) {
+		
+		if (this.stocks.contains(this)) {
+			this.stocks.remove(stock);
+			
+			if (stock.getFridge().equals(this)) {
+				stock.removeFridge();
+			}
+		}
+    }
+	
+	
 	
 }
--- eatsmart/src/main/java/dom/company/eatsmart/model/Stock.java
+++ eatsmart/src/main/java/dom/company/eatsmart/model/Stock.java
@@ -11,6 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 public class Stock {
@@ -20,5 +23,56 @@ public class Stock {
 	private long id;
 	
 	@Embedded
+	@NotNull
 	private Ingredient ingredient;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "FRIDGE_ID")
+	private Fridge fridge;
+
+	public Stock() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	@XmlTransient
+	public Fridge getFridge() {
+		return fridge;
+	}
+
+	public void setFridge(Fridge fridge) {
+		this.fridge = fridge;
+		if (!fridge.getStocks().contains(this)) {
+			fridge.getStocks().add(this);
+		}
+	}
+	
+	public void removeFridge() {
+		
+		Fridge currentFridge = this.getFridge();
+		if (currentFridge != null){
+			this.fridge = null;
+		}
+		if (currentFridge.getStocks().contains(this)) {
+			currentFridge.removeStock(this);
+		}
+	}
+	
+	
+	
+	
 }
