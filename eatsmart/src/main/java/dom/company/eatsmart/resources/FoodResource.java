03bfceaf4e61e7b2005225aa5e339764d3856740
Stefan Specht <stefan_specht@gmx.net>
2016-03-02 04:35:42 +0100

    Adjusted foodresource links

--- eatsmart/src/main/java/dom/company/eatsmart/resources/FoodResource.java
+++ eatsmart/src/main/java/dom/company/eatsmart/resources/FoodResource.java
@@ -1,6 +1,7 @@
 package dom.company.eatsmart.resources;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
@@ -18,8 +19,12 @@ import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import org.glassfish.jersey.server.Uri;
+
+import dom.company.eatsmart.exception.InternalServerErrorException;
 import dom.company.eatsmart.model.Food;
 import dom.company.eatsmart.service.FoodService;
 
@@ -28,9 +33,9 @@ import dom.company.eatsmart.service.FoodService;
 @Produces(MediaType.APPLICATION_JSON)
 public class FoodResource {
 	
+	FoodService foodService = new FoodService();
 	private long parentFoodId;	
-	
-	
+		
 	public FoodResource() {
 		this.parentFoodId = 0;
 	}
@@ -38,10 +43,6 @@ public class FoodResource {
 	public FoodResource(long parentFoodId) {
 		this.parentFoodId = parentFoodId;
 	}
-
-
-
-	FoodService foodService = new FoodService();
 	
 	@GET
 	public Response getFoods(@PathParam("userId") long userId, @Context UriInfo uriInfo) {
@@ -50,7 +51,7 @@ public class FoodResource {
 			GenericEntity<List<Food>> entity = new GenericEntity<List<Food>>(foods) {};
 			
 			return Response.ok(entity)
-					.links(getLinks(uriInfo, "GET"))
+					.links(getLinks(uriInfo, "GET_ALL"))
 					.build();
 		}
 		else {
@@ -58,7 +59,7 @@ public class FoodResource {
 			GenericEntity<List<Food>> entity = new GenericEntity<List<Food>>(foods) {};
 			
 			return Response.ok(entity)
-					.links(getLinks(uriInfo, "GET"))
+					.links(getLinks(uriInfo, "GET_CHILDFOOD"))
 					.build();
 		}		
 		
@@ -117,25 +118,42 @@ public class FoodResource {
 	}
 	
 	private Link[] getLinks(UriInfo uriInfo, String method) {
-		Link self = Link.fromUri(uriInfo.getAbsolutePath()).rel("self").param("verb", "GET,PUT,POST").build();
-		Link findByName = Link.fromUri(uriInfo.getAbsolutePathBuilder().replaceQuery("qName=").build()).rel("findByName").param("verb", "GET").build();
-		Link sortByNameAsc = Link.fromUri(uriInfo.getAbsolutePathBuilder().replaceQuery("sort=name").build()).rel("sortByNameAsc").param("verb", "GET").build();
-		Link sortByNameDesc = Link.fromUri(uriInfo.getAbsolutePathBuilder().replaceQuery("sort=-name").build()).rel("sortByNameDesc").param("verb", "GET").build();
-		Link sortByRatingDesc = Link.fromUri(uriInfo.getAbsolutePathBuilder().replaceQuery("sort=-rating").build()).rel("sortByRatingDesc").param("verb", "GET").build();
-		Link sortBySmartRankingDesc = Link.fromUri(uriInfo.getAbsolutePathBuilder().replaceQuery("sort=-SmartRanking").build()).rel("sortBySmartRankingDesc").param("verb", "GET").build();
-		
-		Link newfood = Link.fromUri(uriInfo.getAbsolutePath()).rel("new").param("verb", "POST").build();
-		Link user = Link.fromUri(uriInfo.getAbsolutePathBuilder().path("..").build()).rel("user").param("verb", "GET").build();
-		
+		Link self = Link.fromUri(uriInfo.getAbsolutePath()).rel("self").param("verb", "GET,POST").build();
+		Link all = Link.fromUri(uriInfo.getAbsolutePathBuilder().path("..").build()).rel("all").param("verb", "GET,POST").build();
+		Link newFood = Link.fromUri(uriInfo.getAbsolutePath()).rel("new").param("verb", "POST").build();
+		Link childs = Link.fromUri(uriInfo.getAbsolutePathBuilder().path("childFoods").build()).rel("childs").param("verb", "GET,POST").build();
+		Link parent = Link.fromUri(uriInfo.getAbsolutePathBuilder().path("..").build()).rel("parent").param("verb", "GET,POST").build();
+		Link user = Link.fromUri(this.buildUserUri(uriInfo)).rel("user").param("verb", "GET").build();
 		Link logout = Link.fromUri(uriInfo.getBaseUri()).rel("logout").param("verb", "GET").build();
 		
 		switch (method) {
-			case "GET":
-				return new Link[] {self, findByName, sortByNameAsc, sortByNameDesc, sortByRatingDesc, sortBySmartRankingDesc, newfood, user, logout};
+			case "GET_ALL":
+				return new Link[] {self, newFood, user, logout};
 			case "POST":
 				return new Link[] {self, user, logout};
+			case "GET":
+				return new Link[] {self, all, childs, user, logout};
+			case "GET_CHILDFOOD":
+				return new Link[] {self, newFood, parent, user, logout};
 			default:
 				return new Link[] {};
+		}		
+	}
+	private URI buildUserUri(UriInfo uriInfo) {
+		String currentUri = uriInfo.getAbsolutePath().toString();
+		
+		do {
+			currentUri = currentUri.substring(0,currentUri.lastIndexOf("/"));
+		} while (!currentUri.matches(".+\\/users\\/[0-9]+"));
+		
+		try {
+			URI userUri = new URI(currentUri);
+			return userUri;		
+		} 
+		catch (URISyntaxException ex) {
+			throw new InternalServerErrorException("Could not build URI to user resource");
 		}
+		
+		
 	}
 }
