f5ca529e267a214b0d62850aa68146a2338bd1ca
Stefan Specht <stefan_specht@gmx.net>
2016-03-05 16:49:29 +0100

    Added missing getter/setter for recipe in menuSchedule

--- eatsmart/src/main/java/dom/company/eatsmart/model/MenuSchedule.java
+++ eatsmart/src/main/java/dom/company/eatsmart/model/MenuSchedule.java
@@ -90,6 +90,16 @@ public class MenuSchedule {
 			currentMenu.removeMenuSchedule(this);
 		}
 	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	
+	
 	
 	
 	
